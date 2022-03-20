48ad443f9aa4c61170a461705cf69d72ef32ab2b
kdany25 <kabadany25@gmail.com>
2022-03-20 22:21:08 +0200

    high order

--- react_ui/src/container/Work/Work.jsx
+++ react_ui/src/container/Work/Work.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Work.scss'
-
+import {AiFillEye , AiFillGithub} from 'react-icons/ai'
+import {motio} from "framer-motion"
+import { AppWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
+import './Work.scss';
 const Work = () => {
   return (
-    <div>Work</div>
+    <> <h2 className="head-text">
+    My creative <span>Portofolio</span>Section
+  <div className='app__work-filter'>{["UI/UX","web App" , "Mobile App" , "React JS" , "All"].map((item,index)=>())}</div>
+  </h2></>
   )
 }
 
